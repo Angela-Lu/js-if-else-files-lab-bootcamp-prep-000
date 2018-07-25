7547afc3398f8be6572751ce9d314f1234f3eeb1
Angela Lu <angela.lu0919@gmail.com>
2018-07-25 02:58:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var height = "74"
 if (name === "Susan") {
   console.log("The name is correct")
 } else {
-  console.log("The name wasn;t Susan")
+  console.log("The name wasn't Susan")
 }
 
 // Don't worry about this
